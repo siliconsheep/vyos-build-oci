56d5b17794cdebf063d38d0be2ef30a104b2e6b2
Dieter Bocklandt <dieterbocklandt@gmail.com>
2022-11-04 17:47:20 +0100

    feat: first test with OCI integratation

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-./configure --architecture amd64 --build-by "dieterbocklandt@gmail.com"
-sudo make iso
+VYOS_VERSION_NUMBER=$(cat data/versions | jq -r 'first(.[])')
+
+./configure \
+  --architecture amd64 \
+  --build-by "dieterbocklandt@gmail.com" \
+  --build-type release \
+  --version "${VYOS_VERSION_NUMBER}"
+
+sudo make oracle
