37b679ec16e4468e97d529381c6a6a59a51933a9
Dieter Bocklandt <siliconsheep@github.com>
2022-11-05 00:16:48 +0100

    fix: hopefully final fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
-VYOS_VERSION_NUMBER=$(cat data/versions | jq -r 'first(.[])')
-
 ./configure \
   --architecture amd64 \
   --build-by "dieterbocklandt@gmail.com" \
   --build-type release \
-  --version "${VYOS_VERSION_NUMBER}"
+  --version "${VYOS_VERSION_NO}"
 
 sudo make oracle
